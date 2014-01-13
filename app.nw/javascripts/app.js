7ceb297b04bea4dcaba045faac9af7888764f44a
Tom Moor <tom.moor@gmail.com>
2014-01-13 10:40:22 -0500

    First commit

---
+++ app.nw/javascripts/app.js
@@ -0,0 +1,31 @@
+var video = document.getElementById("photobooth");
+var fs = require('fs');
+var gui = require('nw.gui');
+gui.Window.get().showDevTools();
+
+navigator.webkitGetUserMedia({video:true, audio:true},
+    function(stream) {
+      video.src = window.webkitURL.createObjectURL(stream);
+    }
+);
+
+video.onclick = function() {
+	var filename = (new Date()).getTime();
+	var canvas = document.createElement('canvas');
+	var context = canvas.getContext('2d');
+
+	canvas.width = video.videoWidth;
+	canvas.height = video.videoHeight;
+	context.drawImage(video, 0, 0);
+
+	var uri = canvas.toDataURL('image/jpeg');
+	var data = uri.split(',')[1];
+  var buffer = new Buffer(data, 'base64');
+  var destination = '/Users/tom/Desktop/' + filename + '.jpg';
+
+  fs.writeFile(destination, buffer, 'binary', function(err) {
+  	if (err) return console.log(err);
+  	
+  	console.log('Photo saved as ', destination);
+  });
+};
